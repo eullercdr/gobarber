4eba8f7865b455cb603a27fa6e0c733571adc5fa
Euller Cristian <eullercdr@gmail.com>
2020-02-03 14:36:17 -0300

    Validações de data, não permite agendamentos no mesmo horário, ou na mesma hora

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,6 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -29,6 +31,32 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers ' });
+    }
+
+    /**
+     * Check if date is not past
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    console.log(hourStart);
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(401).json({ error: 'Past date are not permited' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(401).json({ error: 'Appoint date is not available' });
     }
 
     const appointment = await Appointment.create({
