53d1dbd679935bd05429fb4f7e83939e925d39e6
Euller Cristian <eullercdr@gmail.com>
2020-02-03 19:16:42 -0300

    Gerando notificações ao prestador a cada agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { pt } from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -69,8 +71,6 @@ class AppointmentController {
      */
     const hourStart = startOfHour(parseISO(date));
 
-    console.log(hourStart);
-
     if (isBefore(hourStart, new Date())) {
       return res.status(401).json({ error: 'Past date are not permited' });
     }
@@ -94,6 +94,19 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(hourStart, "'dia' dd 'de' MMMM',ás' H:mm'h'", {
+      locale: pt,
+    });
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
