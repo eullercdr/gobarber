8d6288f116ab232800e6c29b396cf5a688f35cac
Euller Cristian <eullercdr@gmail.com>
2020-02-15 11:10:00 -0300

    Configurando envio de e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '25',
+  secure: false,
+  auth: {
+    user: 'abef6303e7d96e',
+    pass: 'f6ca7851cfd551',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
